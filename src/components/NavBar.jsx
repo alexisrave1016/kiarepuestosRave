0a79a947cb0fe6dd84b30082068b4d574e58bbf6
alexisRave <alexis9876@hotmail.com>
2022-06-14 23:54:24 -0500

    inicio

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='container-navBar'>
+        <div className="navBar">
+            <div className="logo-navBar">
+                <img src="" alt="" />
+            </div>
+
+            <div className="buscador"></div>
+
+            <div className="login">
+                <img src="logoTrasparente.png" alt="" />
+                <span>Iniciar secion</span>
+                <span>Registrate</span>
+            </div>
+        </div>
+    </div>
+  )
+}
